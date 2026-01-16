cde44f593a0bcdd1a09a995f9393b664fabf4e7d
EGgery <eguzman0086@mymail.lausd.net>
2026-01-16 14:34:37 -0800

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by GUZMAN, ETHAN on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordGardenTests/WordGardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenTests.swift
+//  WordGardenTests
+//
+//  Created by GUZMAN, ETHAN on 1/16/26.
+//
+
+import Testing
+@testable import WordGarden
+
+struct WordGardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
